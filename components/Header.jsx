352270d75bbee650a8baa042b33ee38fc1cb9a32
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2023-12-27 00:45:18 -0300

    feat: header content

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,53 @@
 import "./Header.css"
+import { IoMdArrowDropdown } from "react-icons/io";
+import {useRef, useState, useEffect} from "react";
 
 const Header = () => {
+  const languageRef = useRef(null)
+  const [visible, setVisible] = useState(false)
+
+  const showHide = () => {
+
+    const language = languageRef.current.getElementsByClassName("dropdown")
+      
+    if (visible) {
+      language[0].style.visibility = "hidden";
+      setVisible(!visible)
+    } else {
+      language[0].style.visibility = "visible";
+      setVisible(!visible)
+    }
+
+  }
+
+  const handleClickOutside = (event) => {
+    if (languageRef.current && !languageRef.current.contains(event.target)) {
+      setVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  
   return (
-    <div className="header">Header</div>
+    <div className="header">
+      <nav className="nav-content">
+        <a href="/">
+          Home
+        </a>
+        <div ref={languageRef} className="idioma">
+        <div onClick={() => showHide()} className="selected-language">Idioma <IoMdArrowDropdown className="arrow-down"/></div>
+          <ul className="dropdown">
+            <li><a>Português (PT-BR)</a></li>
+            <li><a>Inglês (EN-US)</a></li>
+          </ul>
+        </div>
+      </nav>      
+    </div>
   )
 }
 
